d7092c0c77ff434471bbbf64c147b0c40ba7a97b
Діана Тепер <dianateper333@gmail.com>
2019-10-26 13:11:37 +0300

    update yacht_detail

---
+++ root/php/yacht_detail.php
@@ -0,0 +1,479 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
+
+		<link rel="stylesheet" type="text/css" href="../css/slick/slick.css"/>
+		<link rel="stylesheet" type="text/css" href="../css/slick/slick-theme.css"/>
+		
+		<link rel="stylesheet" href="..\css\reset.css" type="text/css"/>
+		<link rel="stylesheet" href="..\css\header.css" type="text/css"/>
+		<link rel="stylesheet" href="..\css\footer.css" type="text/css"/>
+		<link rel="stylesheet" href="..\css\style.css" type="text/css">
+		<link rel="stylesheet" href="..\css\yacht_detail.css" type="text/css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	</head>
+	<body>
+		<!--------------------------------------Header------------------------------------------------------------->
+		<div>
+			<?php include 'header.php';?>		
+		</div>
+		<!--------------------------------------END Header------------------------------------------------------------->
+
+		<div>
+			<div class="mycontainer">
+				<div>
+					<h3>2015 Princess V62-S</h3>
+				</div>
+				<div class="small-container">
+				<div class="yacht-slide">
+					<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+						  <div class="carousel-inner">
+						    <div class="carousel-item active">
+						      <img class="d-block" src="..\img\Princess\Princess.png" alt="Princess">
+						    </div>
+						    <div class="carousel-item">
+						      <img class="d-block" src="..\img\Princess\Princess_1.png" alt="Princess">
+						    </div>
+						    <div class="carousel-item">
+						      <img class="d-block" src="..\img\Princess\Princess_2.png" alt="Princess">
+						    </div>
+						     <div class="carousel-item">
+						      <img class="d-block" src="..\img\Princess\Princess_3.png" alt="Princess">
+						    </div>
+						     <div class="carousel-item">
+						      <img class="d-block" src="..\img\Princess\Princess_4.png" alt="Princess">
+						    </div>
+						     <div class="carousel-item">
+						      <img class="d-block" src="..\img\Princess\Princess_5.png" alt="Princess">
+						    </div>
+						     <div class="carousel-item">
+						      <img class="d-block" src="..\img\Princess\Princess_6.png" alt="Princess">
+						    </div>
+						     <div class="carousel-item">
+						      <img class="d-block" src="..\img\Princess\Princess_7.png" alt="Princess">
+						    </div>
+						     <div class="carousel-item">
+						      <img class="d-block" src="..\img\Princess\Princess_8.png" alt="Princess">
+						    </div>
+						     <div class="carousel-item">
+						      <img class="d-block" src="..\img\Princess\Princess_9.png" alt="Princess">
+						    </div>
+						     <div class="carousel-item">
+						      <img class="d-block" src="..\img\Princess\Princess_10.png" alt="Princess">
+						    </div>
+						     <div class="carousel-item">
+						      <img class="d-block" src="..\img\Princess\Princess_11.png" alt="Princess">
+						    </div>
+						  </div>
+						  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+						    <span class="carousel-control-prev-icon" aria-hidden="true"><img src="..\img\arrow_left.png" alt=""></span>
+						    <span class="sr-only">Previous</span>
+						  </a>
+						  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+						    <span class="carousel-control-next-icon" aria-hidden="true"><img src="..\img\arrow_right.png" alt=""></span>
+						    <span class="sr-only">Next</span>
+							</a>
+						</div>	
+				</div>
+			</div>
+		</div>
+		<div class="rect rect-orange"></div>
+		<div class="rect rect-blue"></div>
+		<div class="rect rect-dark-blue"></div>
+		<div class="rect rect-dark-blue rect-dark-blue-next"></div>
+		<div class="mycontainer">
+			<div class="small-container">
+				<div class="yacht-detail-slide myrow">
+						 <nav>
+						        <h5><a href="#page-1" class="scrollitem ">Description</a></h5>
+						        <h5><a href="#page-2" class="scrollitem">Specifications</a></h5>
+						        <h5><a href="#page-3" class="scrollitem">Gallery</a></h5>
+						        <h5><a href="#page-4" class="scrollitem">Rent</a></h5>
+
+						    </nav>
+						    <a href=""><img  alt="" id="slide-arrow" src="..\img\yacht_detail\line_dotes1.png"></a>
+						<div class="wrapper">
+<!--------------------------------------------------------------------PAGE 1 ------------------------------------------------------------------->							
+						   
+						    <div id="page-1" class="page"> 
+						         <p>Ut mauris nulla sed.</p>
+							      
+							     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Metus eget maecenas faucibus dolor aenean urna, nisi. Diam eu lacus urna morbi molestie dui. Facilisis id vestibulum viverra nulla tellus urna, eget dui, leo. Odio justo velit scelerisque leo lectus eget suspendisse vitae. Eget tincidunt felis dui pellentesque aenean. Fermentum sit vestibulum elit suspendisse eget at porttitor id. Elit pellentesque blandit integer est eget pharetra integer. </p>
+							     <p>Purus varius tellus, nulla lectus tempor, dui ac. Vestibulum scelerisque eget urna, dignissim turpis tortor. Varius aliquet diam nullam tempus. Tellus phasellus diam sodales non non morbi scelerisque pulvinar. Aenean id orci, scelerisque in tempus ut adipiscing vulputate. Est varius erat pellentesque.</p>
+							  
+							</div>
+<!--------------------------------------------------------------------PAGE 2 ------------------------------------------------------------------->	
+						    <div id="page-2" class="page page-hide">  
+						        
+						        <div>  	
+						        	<table class="yacht-detail-table">
+						        	<thead><tr><td colspan="4">Basic information</td></tr></thead>
+						        	<tbody>
+						        		<tr>
+						        			<td>Manufacturer:</td>
+						        			<td>Princess</td>
+						        			<td>Vessel Name:</td>
+						        			<td>Swept Away</td>
+						        		</tr>
+						        		<tr>
+						        			<td>Model:</td>
+						        			<td>V62-5</td>
+						        			<td>Boat Type:</td>
+						        			<td>Motor Yacht</td>
+						        		</tr>
+						        		<tr>
+						        			<td>Year:</td>
+						        			<td>2015</td>
+						        			<td>Hull Material:</td>
+						        			<td>Fiberglass</td>
+						        		</tr>
+						        		<tr>
+						        			<td>Category:</td>
+						        			<td>Power</td>
+						        			<td>Hull Color:</td>
+						        			<td>Navy blue</td>
+						        		</tr>
+						        		<tr>
+						        			<td>Condition:</td>
+						        			<td>Used</td>
+						        			<td>HIN</td>
+						        			<td>VSC621301415</td>
+						        		</tr>
+						        	</tbody>
+
+						        	<thead><tr><td  colspan="4">Dimensions & Weight</td></tr></thead>
+						        	<tr>
+						        		<td>Length:</td>
+						        		<td>62 ft - 18.9 meter</td>
+						        		<td>Draft - max:</td>
+						        		<td>-</td>
+						        	</tr>
+						        	<tr>
+						        		<td>LOA:</td>
+						        		<td>63 ft 6 in - 19.35 meter</td>
+						        		<td>Bridge Clearance</td>
+						        		<td>-</td>
+						        	</tr>
+						        	<tr>
+						        		<td>Beam:</td>
+						        		<td>16 ft 5 in - 5 meter</td>
+						        		<td>Dry Weight</td>
+						        		<td>-</td>
+						        	</tr>
+						        	
+						        	<thead><tr><td  colspan="4">Engine</td></tr></thead>
+						        	<tr>
+						        		<td>Make:</td>
+						        		<td>Caterpillar</td>
+						        		<td>Engine Type:</td>
+						        		<td>Inboard</td>
+						        	</tr>
+						        	<tr>
+						        		<td>Model:</td>
+						        		<td>C-18 Acert</td>
+						        		<td>Drive Type:</td>
+						        		<td></td>
+						        	</tr>
+						        	<tr>
+						        		<td>Engine(s):</td>
+						        		<td>2</td>
+						        		<td>Fuel Type</td>
+						        		<td>Diesel</td>
+						        	</tr>
+						        	<tr>
+						        		<td>Hours</td>
+						        		<td>350</td>
+						        		<td>Horsepower</td>
+						        		<td>1150 (individual), 2300</td>
+						        	</tr>
+						        	<tr>
+						        		<td>Cruise Speed:</td>
+						        		<td>-</td>
+						        		<td></td>
+						        		<td>(comdined)</td>
+						        	</tr>
+						        	<tr>
+						        		<td>Range:</td>
+						        		<td>-</td>
+						        		<td>Max Speed:</td>
+						        		<td>-</td>
+						        	</tr>
+						        	<tr>
+						        		<td>Joystick Control:</td>
+						        		<td>No</td>
+						        		<td></td>
+						        		<td></td>
+						        	</tr>
+						        	<thead><tr><td  colspan="4">Tank capacities</td></tr></thead>
+						        	<tr>
+						        		<td>Fuel Tank:</td>
+						        		<td>900 gallons - 1 tank(s)</td>
+
+						        	</tr>
+						        	<tr>
+						        		<td>Fresh Water Tank:</td>
+						        		<td>130 gallons - 1 tank(s)</td>
+						        		
+						        	</tr>
+						        	<tr>
+						        		<td>Holding Tank</td>
+						        		<td>73 gallons - 1 tank(s)</td>
+						        		
+						        	</tr>
+
+						        	<thead><tr><td  colspan="4">Accommodations</td></tr></thead>
+						        	<tr>
+						        		<td>Total Cabins:</td>
+						        		<td>2</td>
+						        		<td>Crew Cabins:</td>
+						        		<td>-</td>
+						        	</tr>
+						        	<tr>
+						        		<td>Total Berths:</td>
+						        		<td>2</td>
+						        		<td>Crew Berths:</td>
+						        		<td>-</td>
+						        	</tr>
+						        	<tr>
+						        		<td>Total Sleeps:</td>
+						        		<td>1</td>
+						        		<td>Crew Sleep:</td>
+						        		<td>-</td>
+						        	</tr>
+						        	<tr>
+						        		<td>Total Heads:</td>
+						        		<td>2</td>
+						        		<td>Crew Heads:</td>
+						        		<td>-</td>
+						        	</tr>
+						        	<tr>
+						        		<td>Captains Cabin:</td>
+						        		<td>No</td>
+						        		<td></td>
+						        		<td></td>
+						        	</tr>
+						        	</table>
+						        </div>
+						    </div>
+ <!--------------------------------------------------------------------PAGE 3 ------------------------------------------------------------------->	
+						    <div id="page-3" class="page page-hide"> 
+						       
+						      
+								
+
+
+
+						       <div class="responsive f-l w-825">
+									 <div><img src="../img/Princess/Princess_9.png" alt="" width="195px" height="130px;"></div>
+									  <div><img src="../img/Princess/Princess_10.png" alt="" width="195px" height="130px;"></div>
+									  <div><img src="../img/Princess/Princess_11.png" alt="" width="195px" height="130px;"></div>
+									  <div><img src="../img/Princess/Princess.png" alt="" width="195px" height="130px;"></div>
+									  <div><img src="../img/Princess/Princess_1.png" alt="" width="195px" height="130px;"></div>
+									  <div><img src="../img/Princess/Princess_2.png" alt="" width="195px" height="130px;"></div>
+									  <div><img src="../img/Princess/Princess_3.png" alt="" width="195px" height="130px;"></div>
+									  <div><img src="../img/Princess/Princess_4.png" alt="" width="195px" height="130px;"></div>
+									
+
+
+
+
+						      		  </div>
+
+						     
+									<div class=" responsivertl f-r w-825" dir="rtl">
+										 <div><img src="../img/Princess/Princess_5.png" alt="" width="195px" height="130px;"></div>
+										  <div><img src="../img/Princess/Princess_6.png" alt="" width="195px" height="130px;"></div>
+										  <div><img src="../img/Princess/Princess_7.png" alt="" width="195px" height="130px;"></div>
+										  <div><img src="../img/Princess/Princess_8.png" alt="" width="195px" height="130px;"></div>
+										  <div><img src="../img/Princess/Princess_9.png" alt="" width="195px" height="130px;"></div>
+										  <div><img src="../img/Princess/Princess_10.png" alt="" width="195px" height="130px;"></div>
+										  <div><img src="../img/Princess/Princess_11.png" alt="" width="195px" height="130px;"></div>
+										  <div><img src="../img/Princess/Princess.png" alt="" width="195px" height="130px;"></div>
+									</div>
+						        
+						        
+									<div class="responsive f-l w-825">
+									 <div><img src="../img/Princess/Princess_9.png" alt="" width="195px" height="130px;"></div>
+									  <div><img src="../img/Princess/Princess_10.png" alt="" width="195px" height="130px;"></div>
+									  <div><img src="../img/Princess/Princess_11.png" alt="" width="195px" height="130px;"></div>
+									  <div><img src="../img/Princess/Princess.png" alt="" width="195px" height="130px;"></div>
+									  <div><img src="../img/Princess/Princess_1.png" alt="" width="195px" height="130px;"></div>
+									  <div><img src="../img/Princess/Princess_2.png" alt="" width="195px" height="130px;"></div>
+									  <div><img src="../img/Princess/Princess_3.png" alt="" width="195px" height="130px;"></div>
+									  <div><img src="../img/Princess/Princess_4.png" alt="" width="195px" height="130px;"></div>
+									
+
+
+
+
+						      		  </div>
+
+
+
+
+
+
+							
+
+
+
+						       
+
+
+							</div>
+ <!--------------------------------------------------------------------PAGE 4 ------------------------------------------------------------------->	
+						    <div id="page-4" class="page page-hide"> 
+						        <div class="small-container myrow">
+						        	<div class="calendar"></div>
+						        	<div>
+						        		<div>
+						        			<p>Price per day:</p>
+						        			<p>$230,000</p>
+						        		</div>
+						        		<div>
+						        			<p></p>
+						        			<p>x2</p>
+						        		</div>
+						        		<div>
+						        			<p>Additional services</p>
+						        			<p></p>
+						        		</div>
+						        		<div>
+						        			
+						        		</div>
+						        	</div>
+						        </div>
+						    </div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+		
+		<!----------------------------------------------------FOOTER----------------------------------------------------------------->
+		<div>
+			<?php include 'footer.php';?>	
+		</div>
+
+		<!-------------------------------------------------END FOOTER---------------------------------------------------------------->
+		<script src="../js/yacht_detail.js"></script>
+		<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+		<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+		<script type="text/javascript" src="../css/slick/slick.min.js"></script>
+
+
+		<script type="text/javascript">
+
+
+
+
+
+
+
+		    $(document).ready(function(){
+
+		    
+
+
+
+					$('.responsive').slick({
+					dots: false,
+					infinite: true,
+					speed: 300,
+					slidesToShow: 4,
+					slidesToScroll: 4,
+					  
+ 					prevArrow:"<img class='a-left control-c prev slick-prev' src='../img/yacht_detail/dotes_left.png'>",
+ 					nextArrow:"<img class='a-right control-c next slick-next' src='../img/yacht_detail/dotes_right.png'>",
+
+
+					responsive: [
+					  {
+					    breakpoint: 1024,
+					    settings: {
+					    slidesToShow: 3,
+					    slidesToScroll: 3,
+					    infinite: true,
+					    dots: false
+					   }
+					    },
+					    {
+					      breakpoint: 600,
+					      settings: {
+					        slidesToShow: 2,
+					        slidesToScroll: 2
+					      }
+					    },
+					    {
+					      breakpoint: 480,
+					      settings: {
+					        slidesToShow: 1,
+					        slidesToScroll: 1
+					      }
+					    }
+					  ]
+					});
+
+
+		     $('.responsivertl').slick({
+		     		rtl:true,
+					dots: false,
+					infinite: true,
+					speed: 300,
+					slidesToShow: 4,
+					slidesToScroll: 4,
+				
+ 					nextArrow:"<img class='a-right control-c next slick-next' src='../img/yacht_detail/dotes_left.png'>",
+
+					responsive: [
+					  {
+					    breakpoint: 1024,
+					    settings: {
+					    slidesToShow: 3,
+					    slidesToScroll: 3,
+					    infinite: true,
+					    dots: false
+					   }
+					    },
+					    {
+					      breakpoint: 600,
+					      settings: {
+					        slidesToShow: 2,
+					        slidesToScroll: 2
+					      }
+					    },
+					    {
+					      breakpoint: 480,
+					      settings: {
+					        slidesToShow: 1,
+					        slidesToScroll: 1
+					      }
+					    }
+					  ]
+					});
+					$('.container-gallery').gallery({
+					  customControls: {
+					    prevButton: prevContent,
+					    nextButton: nextContent
+					  }
+					});
+			    
+		      
+		    });
+ 	 </script>
+
+ 	 <script>
+
+
+		</script>
+	</body>
+</html>
